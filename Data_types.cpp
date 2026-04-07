21e22ee14f905047a8e21359ff709619db612c29
Nisha Bhadu <nisha9499206673@gmail.com>
2026-04-08 00:07:43 +0530

    added Data_types

---
+++ Data_types.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // int, long, long long, float, double
+    // string and getline
+    // char
+    char ch;
+    cin >> ch;
+    cout << ch;
+    return 0;
+}  
