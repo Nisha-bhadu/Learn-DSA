4cbbcdfe08660277901f3bba8365e6be844f02b2
Nisha Bhadu <nisha9499206673@gmail.com>
2026-06-07 11:45:36 +0530

    added variable_problem

---
+++ variable_problem.cpp
@@ -0,0 +1,17 @@
+//Write a program that declares and initializes four variables (a, b, c, and d) to store the values 10, 3.14, 'A', and true, respectively. Print each variable in a separate line.
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int a=10;
+    float b= 3.14;
+    char c='A';
+    bool d=true;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<boolalpha<<d;
+    return 0;
+}
